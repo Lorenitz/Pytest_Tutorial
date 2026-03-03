e6e6b3590c6085ee12f217bf7d8e705bbd5c0547
“Lorenitz” <lorena.g.saar@gmail.com>
2026-03-03 14:41:04 +0100

    feat: adding mock tests

---
+++ pytest-fcc/tests/test_service.py
@@ -0,0 +1,22 @@
+import pytest
+import source.service as service
+from source.service import get_user_from_db, get_users
+
+
+import unittest.mock as mock
+
+@mock.patch("source.service.get_user_from_db")
+def test_get_user_from_db(mock_get_user_from_db):
+    mock_get_user_from_db.return_value = "Alice"
+    user_name = get_user_from_db(1)
+    assert user_name == "Alice"
+
+
+@mock.patch("requests.get")
+def test_get_users(mock_get):
+    mock_response = mock.Mock()
+    mock_response.status_code = 200
+    mock_response.json.return_value = {"id": 1, "name": "John Doe"}
+    mock_get.return_value = mock_response
+    data = service.get_users()
+    assert data == {"id": 1, "name": "John Doe"}
