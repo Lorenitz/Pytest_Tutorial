4f2de3271087caa45bd3ad5a7b384379071f1cf3
“Lorenitz” <lorena.g.saar@gmail.com>
2026-03-03 12:22:24 +0100

    feat: adding extra examples with parametrize

--- pytest-fcc/tests/test_square.py
+++ pytest-fcc/tests/test_square.py
@@ -12,3 +12,13 @@ def test_multiple_square_areas(side_length, expected_area):
     """Test multiple square areas with different side lengths."""
     square = shapes.Square(side_length)
     assert square.area() == expected_area
+
+@pytest.mark.parametrize("side_length, expected_perimeter", [
+    (3, 12),
+    (4, 16),
+    (5, 20)
+])
+def test_multiple_perimeters(side_length, expected_perimeter):
+    """Test multiple square perimeters with different side lengths."""
+    square = shapes.Square(side_length)
+    assert square.perimeter() == expected_perimeter
