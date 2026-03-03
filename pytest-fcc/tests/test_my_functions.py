c4e06f58dd8b397d0b77cd58f767050e573d480f
“Lorenitz” <lorena.g.saar@gmail.com>
2026-03-03 11:03:10 +0100

    feat: adding mark to pytest

--- pytest-fcc/tests/test_my_functions.py
+++ pytest-fcc/tests/test_my_functions.py
@@ -1,4 +1,5 @@
 import pytest
+import time
 import source.my_functions as my_functions
 
 
@@ -16,7 +17,19 @@ def test_divide_by_zero():
         my_functions.divide(10, 0)
     assert True
 
+@pytest.mark.slow # Useful to use when you have a test that takes a long time to run, and you want to be able to easily skip it when running your tests.
+def test_very_slow_test():
+    time.sleep(5)
+    result = my_functions.divide(10, 5)
+    assert result == 2
 
-def test_add_strings():
-    result = my_functions.add("I Like ", "Cookies")
-    assert result == "I Like Cookies"
+
+@pytest.mark.skip(reason="This feature is broken") # Useful to use when you have a test that is not ready yet, and you want to be able to easily skip it when running your tests.
+def test_add():
+    assert my_functions.add(1, 4) == 5
+
+
+@pytest.mark.xfail(reason="We know we cannot divide by zero")
+# Useful to use when you have a test that is expected to fail, and you want to be able to easily mark it as such when running your tests.
+def test_divide_by_zero():
+    my_functions.divide(10, 0)
